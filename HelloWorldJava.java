eac8f98499630a160cb8e357eb95e8710a8bc7ad
LakhouaMehdi <Lakhoua_Mehdi@yahoo.com>
2017-02-24 18:24:25 -0800

    Adding actual code
    source code hello world

---
+++ HelloWorldJava.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.util.*;
+import java.io.*;
+
+public class HelloWorld
+{
+	
+	public static void main(String [] args )
+	{   
+	 
+     System.Out.println("Hello World.");
+	}
+	
+ 
+	  
+	
+	
+	
+}
